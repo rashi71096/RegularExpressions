32448731d0c3b363bf775fdf71ace6089b67b994
sudarshanusnale <46290676+sudarshanusnale@users.noreply.github.com>
2020-08-13 16:32:22 +0530

    Update emailValidation.py

--- emailValidation.py
+++ emailValidation.py
@@ -4,12 +4,12 @@
 import re
 
 def email_validation():
-        email = input("Enter the email: ")
-        if re.search("[\w._%+-]{1,20}@[\w.-]{2,20}.[A-Za-z]{2,3}", email):
-            print("It is a valid email address")
-            return True
-        else:
-            print('It is not a valid email address')
-            return False
+    email = input("Enter the string to check does it conatain email or not : ")
+    valid_email = re.search("[\w._%+-]{1,20}@[\w.-]{2,20}.[A-Za-z]{2,3}", email)
+    if valid_email is not None:
+        print("This string conatain valid email address and email address is ", valid_email.group())
+        return True
+    else:
+        print('This sting does not contain valid email address')
 
 
